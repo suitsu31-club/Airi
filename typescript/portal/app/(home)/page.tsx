bd2f337598f94c410e30a8e995952c91d00a3608
Nikaidou Haruki <nharuki@pm.me>
2026-09-01 19:48:21 +0900

    Add theme support and HeroUI styling to portal

---
+++ typescript/portal/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from '@heroui/react';
+
+export default function Home() {
+  return (
+      <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <Button>
+          My Button
+        </Button>
+      </div>
+  );
+}
