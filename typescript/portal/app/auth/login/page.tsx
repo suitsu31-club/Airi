65103e7d38ffc41f29dc2ace56ba14a364d5c66a
Nikaidou Haruki <nharuki@pm.me>
2026-09-01 20:00:07 +0900

    Add auth layout and login page to portal

---
+++ typescript/portal/app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import { Card } from "@heroui/react";
+
+export default function LoginPage() {
+  return (
+    <Card>
+      <Card.Header>
+        <Card.Title>Login</Card.Title>
+      </Card.Header>
+      <Card.Content></Card.Content>
+      <Card.Footer />
+    </Card>
+  );
+}
